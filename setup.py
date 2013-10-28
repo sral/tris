3ce5b2f27e14b66683cbacd9d74a1a1fada9cf0e
Lars Djerf <lars.djerf@gmail.com>
2013-10-28 21:40:04 +0100

    Add setup-script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="tris",
+      version="0.1",
+      description="Another shitty Tetris clone.",
+      license="GPLv3",
+      author="Lars Djerf",
+      author_email="lars.djerf@gmail.com",
+      url="http://github.com/sral/tris",
+      packages=find_packages(),
+      include_package_data=True,
+      test_suite="tris.tests",
+      install_requires=["pygame"])
