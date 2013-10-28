7f547a5b851edeec06314fb1d2feb636b672a58e
Lars Djerf <lars.djerf@gmail.com>
2013-10-28 21:37:48 +0100

    Improve rotation.

--- tris/trimino.py
+++ tris/trimino.py
@@ -137,10 +137,10 @@ class TriminoI(Trimino):
                   (1, 0): 2,
                   (2, 0): 2,
                   (3, 0): 2},
-                 {(0, 0): 2, # horizontal
-                  (0, 1): 2,
-                  (0, 2): 2,
-                  (0, 3): 2})
+                 {(1, 0): 2, # horizontal
+                  (1, 1): 2,
+                  (1, 2): 2,
+                  (1, 3): 2})
 
     def __init__(self, x, y, block_sprites):
         Trimino.__init__(self, x, y, self.rotations, block_sprites)
